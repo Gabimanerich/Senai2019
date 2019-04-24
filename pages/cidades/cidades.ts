e167d1a995b7d55bf60638fd3d6bcdcdcc6226bb
Gabimanerich <44350371+Gabimanerich@users.noreply.github.com>
2019-04-23 21:39:54 -0300

    Add files via upload

---
+++ pages/cidades/cidades.ts
@@ -0,0 +1,48 @@
+import { Toasts } from './../../providers/toast';
+import { HttpProvider } from './../../providers/http';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+
+@Component({
+  selector: 'page-cidades',
+  templateUrl: 'cidades.html',
+})
+
+export class CidadesPage {
+  public cidades = [];
+  public bairros = [];
+  public idcidades : any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http : HttpProvider, private toast : Toasts) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CidadesPage');
+
+    this.http.url = 'http://localhost:3000/cidades';
+    this.http.get().subscribe(
+      (data : any) => {
+        this.cidades = data;
+      },
+      (error : any) => {
+        this.toast.showToast("Não foi possivel carregar...");
+      }
+    );
+  }
+
+escolherBairro(){
+
+    this.http.url = 'http://localhost:3000/bairros/' + this.idcidades;
+    this.http.get().subscribe(
+      (data : any ) => {
+        console.log(data);
+        this.bairros = data;
+      }
+
+    )
+  }
+
+
+}
---
+++ pages/pageprincipal/pageprincipal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PageprincipalPage } from './pageprincipal';
+
+@NgModule({
+  declarations: [
+    PageprincipalPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PageprincipalPage),
+  ],
+})
+export class PageprincipalPageModule {}
